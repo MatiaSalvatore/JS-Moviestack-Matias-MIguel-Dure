8a98d62cc350120d0bbf0306973aa7ff7ed4d4d2
MatiaSalvatore <matiasmigueldure@gmail.com>
2024-01-04 07:47:12 -0300

    Proyect Finished

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log("Hello world")
+function card_creator(movies){
+    return `  
+    <article>
+    <h2>${movies.title}</h2>
+    <img src="${movies.image}">
+    <p>${movies.overview}</p>
+</article>
+`
+}
+
+const contenedor = document.getElementById("container")
+
+for (const movie of movies){
+    const card = card_creator(movie);
+    contenedor.innerHTML += card
+}
+
+
+
+
+
+
+function card_creator(movies){
+    return `  
+    <article class=" flex-shrink-0 w-[500px] h-[500px]  border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <img class="rounded-t-lg" src="${movies.image}" alt="" >
+        <section class="w-[450px] h-[175px] m-5 overflow-auto">
+            <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${movies.title}</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">${movies.overview}</p>
+        </section>
+    </article>
+`
+}
