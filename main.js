ee6be49c42c9fe49197e7fd8faef9d0d28ad5323
MatiaSalvatore <matiasmigueldure@gmail.com>
2024-01-08 00:28:25 -0300

    Sprint 2 started

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 const contenedor = document.getElementById("container")
 
-function card_creator(movies){
+function card_creator(movie){
     return `  
     <article class=" flex-shrink-0 w-[500px] h-[500px]  border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img class="rounded-t-lg" src="${movies.image}" alt="" >
+        <img class="rounded-t-lg" src="${movie.image}" alt="" >
         <section class="w-[450px] h-[175px] m-5 overflow-auto">
-            <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${movies.title}</h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">${movies.overview}</p>
+            <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${movie.title}</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">${movie.overview}</p>
         </section>
     </article>
 `
 }
 
-function add_card(array){
+function add_card(array,container){
     for (const movie of array){
         const card = card_creator(movie);
-        contenedor.innerHTML += card
+        container.innerHTML += card
     }    
 }
 
-add_card(movies)
+add_card(movies,contenedor)
