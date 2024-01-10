21b529d9f826b07cec020f7a7ace23551cca90e9
MatiaSalvatore <matiasmigueldure@gmail.com>
2024-01-10 04:05:56 -0300

    Fixes

--- main.js
+++ main.js
@@ -6,15 +6,21 @@ const genreSearch = document.querySelector("#genreSearch")
 
 //Cuando inicia la página se muestran las películas sin filtros:
 let genre_filter = movies
+let name_filter = movies
+
 add_card(genre_filter,contenedor)
 
+
+let filter_array= []
 
 
 
 titleSearch.addEventListener("input",()=>{
     clean_board(contenedor)
-    let array_filtered = genre_filter.filter(x => x.title.toLowerCase().includes(titleSearch.value.toLowerCase()))
-    add_card(array_filtered,contenedor)
+    name_filter = movies.filter(x => x.title.toLowerCase().includes(titleSearch.value.toLowerCase()))
+    filter_array = name_filter.filter(x=> genre_filter.includes(x))
+    add_card(filter_array,contenedor)
+    error_message()
 })
 
 
@@ -22,12 +28,25 @@ genreSearch.addEventListener("change",()=>{
     if (genreSearch.value === "All"){
         clean_board(contenedor)
         genre_filter = movies
-        add_card(genre_filter,contenedor)
+        filter_array = genre_filter.filter(x=> name_filter.includes(x))
+        add_card(filter_array,contenedor)
+        error_message()
     }
     else{
         clean_board(contenedor)
         genre_filter = movies.filter(x => x.genres.includes(genreSearch.value))
-        add_card(genre_filter,contenedor)
+        filter_array = genre_filter.filter(x=> name_filter.includes(x))
+        add_card(filter_array,contenedor)
+        error_message()
     }
-    
 })
+
+
+
+console.log(filter_array)
+
+function error_message(){
+    if (filter_array.length === 0){
+        contenedor.innerText = "Sorry! But there are no matching results"
+    }
+}
