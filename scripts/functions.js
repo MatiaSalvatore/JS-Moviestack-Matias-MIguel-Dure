787c89c14d027074f015bc251525a4a34ba64fbf
MatiaSalvatore <matiasmigueldure@gmail.com>
2024-01-17 09:03:40 -0300

    Aesthetic fixes

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,9 +1,8 @@
 //Creador de cards para peliculas
 
 function card_creator(movie,array_comparison){
-    let test = false
     let card = ``
-    if (test === true){
+    if (array_comparison.includes(movie.id)){
         card = `  
         <article class="flex-shrink-0 w-[300px] border-gray-200 rounded-lg shadow dark:bg-[#222222] dark:border-[#222222] transition ease-in-out  hover:transform hover:scale-110">
             <img class="rounded-t-lg" src="https://moviestack.onrender.com/static/${movie.image}" alt="" >
@@ -11,14 +10,16 @@ function card_creator(movie,array_comparison){
             <div class = "h-[175px] text-wrap overflow-auto m-1 ">
                 <p class="font-normal text-gray-700 dark:text-gray-400 text-justify m-3">${movie.overview}</p>
             </div>
-            <div data-id="${movie.id}" class="flex flex-row content-center items-center place-content-evenly gap-8">
-            <svg data-id="${movie.id}" fav="activated" class="favbtn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-heart-fill" viewBox="0 0 16 16">
+            <div class="flex flex-row content-center items-center place-content-evenly gap-8 ">
+            <div data-id="${movie.id}" class="h-12 w-12 bg-violet-700 rounded-md flex flex-row items-center justify-center">
+            <svg data-id="${movie.id}"  class="pointer-events-none" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-heart-fill" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
-    </svg>
-    
+      </svg>
+      </div>
           <a type="button" href="movie.html?id=${movie.id}" id="moreInfo" class="bg-[#5b21b6] hover:bg-[#c084fc] text-white font-bold py-2 px-4 rounded-full mt-4 mr-7 mb-7">More info</a>
             </div>
             </article>
+    
 `}
     else{
         card = `  
@@ -29,12 +30,11 @@ function card_creator(movie,array_comparison){
                 <p class="font-normal text-gray-700 dark:text-gray-400 text-justify m-3">${movie.overview}</p>
             </div>
             <div class="flex flex-row content-center items-center place-content-evenly gap-8 ">
-            <div data-id="${movie.id} "class="h-12 w-12 bg-black rounded-md flex flex-row items-center justify-center">
-            <svg data-id="${movie.id} "fav="deactivated" class="favbtn" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-heart-fill" viewBox="0 0 16 16">
+            <div data-id="${movie.id}" class="h-12 w-12 bg-gray-950	 rounded-md flex flex-row items-center justify-center">
+            <svg data-id="${movie.id}"  class="pointer-events-none" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-heart-fill" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+      </svg>
       </div>
-    </svg>
-    
           <a type="button" href="movie.html?id=${movie.id}" id="moreInfo" class="bg-[#5b21b6] hover:bg-[#c084fc] text-white font-bold py-2 px-4 rounded-full mt-4 mr-7 mb-7">More info</a>
             </div>
             </article>
@@ -46,19 +46,16 @@ function card_creator(movie,array_comparison){
 
 //Añadir cards a un container
 
-export function add_card(array, container,comparison){
+export function add_card(array, container, comparison){
     for (const movie of array){
         const card = card_creator(movie,comparison);
         container.innerHTML += card
     }    
 }
 
-
 //Borrar container 
+
 export  function clean_board(container){
     container.innerHTML = ""
 }
 
-export function message(){
-    console.log("horray!")
-}
---
+++ scripts/movies_page.js
@@ -0,0 +1,93 @@
+import {add_card, clean_board} from './functions.js'
+
+const contenedor = document.getElementById("container")
+const titleSearch = document.querySelector("#titleSearch")
+const genreSearch = document.querySelector("#genreSearch")
+
+//Cuando inicia la página se muestran las películas sin filtros:
+
+let genre_filter = []
+let name_filter = []
+let filter_array = []
+let movie_catalog = []
+let favs = JSON.parse(localStorage.getItem("favs")) || []
+
+
+//Llamando a la API para las películas:
+
+const apiKey = "0ff70d54-dc0b-4262-9c3d-776cb0f34dbd"
+
+const init = {
+    method: 'GET',
+    headers: {
+        "x-api-key": "0ff70d54-dc0b-4262-9c3d-776cb0f34dbd"
+    }
+}
+
+const api = fetch('https://moviestack.onrender.com/api/movies', init).then(response => response.json()).then(data =>{
+    movie_catalog = data.movies
+    add_card(movie_catalog,contenedor,favs)
+    console.log(movie_catalog)
+    genre_filter = data.movies
+    name_filter = data.movies
+} ).catch(err => console.log(err))
+
+
+//Funciones usadas en la página movies:
+
+function error_message(){
+    if (filter_array.length === 0){
+        contenedor.innerText = "Sorry! But there are no matching results"
+    }
+}
+
+function fav_functionality(){
+    contenedor.addEventListener("click", (e)=>{
+        if (e.target.dataset.id != undefined){
+            let movieId = e.target.dataset.id 
+            let add_fav = movie_catalog.find(movie => movie.id === movieId)
+            if(favs.includes(movieId)){
+                favs = favs.filter(x => x != movieId)
+                e.target.className = "h-12 w-12 bg-gray-950	 rounded-md flex flex-row items-center justify-center"
+            }
+            else {
+                favs.push(movieId)
+                e.target.className = "h-12 w-12 bg-violet-700 rounded-md flex flex-row items-center justify-center"
+            }
+            }
+        localStorage.setItem("favs",JSON.stringify(favs))
+        }
+    )
+}
+
+fav_functionality()
+
+
+//Eventos Input:
+
+titleSearch.addEventListener("input",()=>{
+    clean_board(contenedor)
+    name_filter = movie_catalog.filter(x => x.title.toLowerCase().includes(titleSearch.value.toLowerCase()))
+    filter_array = name_filter.filter(x=> genre_filter.includes(x))
+    add_card(filter_array,contenedor,favs)
+    error_message()
+})
+
+genreSearch.addEventListener("change",()=>{
+    if (genreSearch.value === "All"){
+        clean_board(contenedor)
+        genre_filter = movie_catalog
+        filter_array = genre_filter.filter(x=> name_filter.includes(x))
+        add_card(filter_array,contenedor,favs)
+        error_message()
+    }
+    else{
+        clean_board(contenedor)
+        genre_filter = movie_catalog.filter(x => x.genres.includes(genreSearch.value))
+        filter_array = genre_filter.filter(x=> name_filter.includes(x))
+        add_card(filter_array,contenedor,favs)
+        error_message()
+    }
+})
+
+
